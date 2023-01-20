c7a53c9f120760ec44d91770c83108de3bb695d9
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-01-20 15:04:18 -0500

    230120

---
+++ s0_config_directories.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+top=/media/mcuser/Data2/desimone                        #parent directory
+proj_dir=${top}/ab4240_imaging/resting_state            #project directory
+data_dir=${proj_dir}/raw_data                           #raw data directory
+out_dir=${proj_dir}/data_preproc; mkdir -p ${out_dir}   #output directory
+job_dir=${proj_dir}/jobs; mkdir -p ${job_dir}           #job directory
