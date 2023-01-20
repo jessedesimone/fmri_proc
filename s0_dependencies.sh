398e1346af5e57f612ffeae722c57098ccfe571a
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-01-20 15:01:31 -0500

    230120

--- s0_dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-: 'module to check dependencies'
-echo -n "checking dependencies >>>>>> "
-command -v fsl >/dev/null 2>&1 || { echo >&2 "fsl is not installed.  Aborting."; exit 1; }
-command -v afni >/dev/null 2>&1 || { echo >&2 "afni is not installed.  Aborting."; exit 1; }
-echo "good"
