873cffd2853e9287b04fe73b8711204736d44157
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-01-23 15:03:26 -0500

    230123

--- preproc/s0_config_directories.sh
+++ preproc/s0_config_directories.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+: 'configure directories for data preprocessing stage'
 top=/media/mcuser/Data2/desimone                        #parent directory
 proj_dir=${top}/ab4240_imaging/resting_state            #project directory
 data_dir=${proj_dir}/data_raw                           #raw data directory
