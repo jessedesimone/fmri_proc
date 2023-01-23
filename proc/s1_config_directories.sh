3516b6efbf67d08078c6e025def1c3ef1afba21b
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-01-23 15:04:55 -0500

    230123

---
+++ proc/s1_config_directories.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+: 'configure directories for data processing stage'
+top=/media/mcuser/Data2/desimone                        #parent directory
+proj_dir=${top}/ab4240_imaging/resting_state            #project directory
+data_dir=${proj_dir}/data_raw                           #raw data directory
+out_dir=${proj_dir}/data_proc; mkdir -p ${out_dir}      #output directory
+job_dir=${proj_dir}/jobs; mkdir -p ${job_dir}           #job directory
