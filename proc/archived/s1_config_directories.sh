ee23873aea5870e429a1f8227e9cef7d3572f3ef
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-01-26 12:39:13 -0500

    230126

--- proc/archived/s1_config_directories.sh
+++ proc/archived/s1_config_directories.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 : 'configure directories for data processing stage'
 top=/media/mcuser/Data2/desimone                        #parent directory
+code_dir=${top}/code/fmri_proc/proc                     #directory where code is run
 proj_dir=${top}/ab4240_imaging/resting_state            #project directory
 data_dir=${proj_dir}/data_raw                           #raw data directory
 out_dir=${proj_dir}/data_proc; mkdir -p ${out_dir}      #output directory
 job_dir=${proj_dir}/jobs; mkdir -p ${job_dir}           #job directory
-script_dir=${out_dir}/${sub}/scripts                    #script directory
--- proc/archived/s1_driver.sh
+++ proc/archived/s1_driver.sh
@@ -2,7 +2,7 @@
 gen_error_msg="\
     Driver to run afni_proc.py
     Note: task BOLD, SUIT, physiol extraction not currently operational; to be added in later iterations
-    afni_proc.py parametersn for corresponding options are defined in s1_config_proc.ini
+    afni_proc.py parameters for corresponding options are defined in s1_config_proc.ini
     
     Usage: ./s1_driver.sh [-h] [-c] [-t] [-s] [-p] [-o]
     Arguments
@@ -11,9 +11,9 @@ gen_error_msg="\
     -t  run whole-brain task BOLD
     -s  run SUIT
     -p  run physiol extraction
-    -o  overwrite 
+    -o  overwrite existing output files
     "
-    while getopts ":hctsp" opt; do
+    while getopts ":hctpseo" opt; do
         case ${opt} in
                 h|\?) #help option
                     echo -e "$gen_error_msg"
@@ -25,11 +25,14 @@ gen_error_msg="\
                 t) #WB task BOLD option
                     tflag=1
                     ;;
+                p) #WB with physio extraction option
+                    pflag=1
+                    ;;
                 s) #SUIT option
                     sflag=1
                     ;;
-                p) #physio extraction option
-                    pflag=1
+                e) #SUIT with physio extraction option
+                    eflag=1
                     ;;
                 o) #overwrite
                     oflag=1
@@ -62,32 +65,53 @@ job_file=${job_dir}/run_proc_py.${dt}
 #define subject list
 sub_list=`cat ${proj_dir}/id_lists/test`
 
-#create output directory if not exist
-: 'output directory should exist already is preprocessing was performed'
-#for sub in ${sub_list[@]}; do mkdir -p ${out_dir}/${sub}; done
-
 for sub in ${sub_list[@]}
 do
-    if [ "$oflag" ]
-    then
-        echo "!!! overwriting !!!" 
-        if [ -d ${out_dir}/${sub} ]; then
-            rm -rf ${out_dir}/${sub}
+    #configure output files
+    out_file=${out_dir}/${sub}/afni.proc.${dt}.out
+    script_file=${out_dir}/${sub}/afni.proc.${dt}.script
 
-    fi
+    #configure input files
+    : 'define epi and ant files; exit code if infiles do not exist'
+    epi_file=${data_dir}/${sub}/${sub}.RS1.nii
+    anat_file=${out_dir}/${sub}/${sub}.spgr2.nii
 
-
-
-
-    if [ "$cflag" ]
-    then
-        echo "*** connectivity analysis ***" 
-        source s0_deoblique.sh 
+    if [[ -f ${epi_file} ]] && [[ -f ${anat_file} ]]; then
+        : 'proceed with data processing if files exist'
+        if [ "$oflag" ]; then
+        echo "!!! overwriting output directory !!!" 
+        : 'remove all files except for anat infile, which
+        was created during s0 data preprocessing'
+        cd ${out_dir}/${sub}
+        shopt -s extglob
+        rm -v !(${sub}.spgr2.nii)
+        cd ${code_dir}
+        fi
+        if [ "$cflag" ]; then
+        echo "preparing data for connectivity processing"
+        fi
+    else
+        echo "!!! ERROR !!! anat and/or epi infiles not found; quitting"
+        exit 1
     fi
 
 done
 
+: '
+    if [ "$cflag" ]; then
+        : ' source connectivity afni_proc.py option from config file' 
+        #-------------afni_proc.py-------------
+        source s1_config_proc.ini; connectivity_config
+        echo "tcsh -xef ${script_file} | tee ${out_file}" >> ${job_file}
+        echo "connectivity afni_proc.py script created for ${sub}"
+    fi
+
+
+
 #run job file
-source ${job_file}
+: 'this will run the afni_proc.py script_file created for each subject'
+chmod +x ${job_file}        #make job file executable
+#source ${job_file}          #run job file
 
 echo "s1_driver.sh finished" 
+'
